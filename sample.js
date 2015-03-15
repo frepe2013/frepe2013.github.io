8852081083337f1e3443c8b7dc5f4e752f59bfde
frepe2013 <needvitneedfgt@gmail.com>
2015-03-15 15:00:29 +0900

    multi map

--- sample.js
+++ sample.js
@@ -1,20 +1,19 @@
 function initialize() {
-  var useragent = navigator.userAgent;
-  var mapdiv = document.getElementById("map_canvas");
+  var latlng1 = new google.maps.LatLng(35.697456,139.702148);
+  var latlng2 = new google.maps.LatLng(34.694203,135.502625);
 
-  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
-    mapdiv.style.width = '100%';
-    mapdiv.style.height = '100%';
-  } else {
-    mapdiv.style.width = '500px';
-    mapdiv.style.height = '250px';
-  }
-
-  var latlng = new google.maps.LatLng(35.539001,134.228468);
-  var opts = {
+  var opts1 = {
     zoom: 13,
-    center: latlng,
+    center: latlng1,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(mapdiv, opts);
+
+  var opts2 = {
+    zoom: 13,
+    center: latlng2,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map1 = new google.maps.Map(document.getElementById("map_canvas1"), opts1);
+  var map2 = new google.maps.Map(document.getElementById("map_canvas2"), opts2);
 }
