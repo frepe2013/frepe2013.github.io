08399c1ab80227f7219114653d11052e20e90b01
frepe2013 <needvitneedfgt@gmail.com>
2015-03-15 14:47:52 +0900

    ブラウザ判定

--- sample.js
+++ sample.js
@@ -1,9 +1,20 @@
 function initialize() {
-  var latlng = new google.maps.LatLng(35.361056,138.731918);
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map_canvas");
+
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+  } else {
+    mapdiv.style.width = '500px';
+    mapdiv.style.height = '250px';
+  }
+
+  var latlng = new google.maps.LatLng(35.539001,134.228468);
   var opts = {
-    zoom: 10,
+    zoom: 13,
     center: latlng,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById("map_canvas"), opts);
+  var map = new google.maps.Map(mapdiv, opts);
 }
