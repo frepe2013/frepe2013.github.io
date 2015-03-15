8f53158e5429ac904a9ede72f54c973eec1100eb
frepe2013 <needvitneedfgt@gmail.com>
2015-03-15 14:30:47 +0900

    外部スクリプト化

---
+++ sample.js
@@ -0,0 +1,9 @@
+function initialize() {
+  var latlng = new google.maps.LatLng(35.709984,139.810703);
+  var opts = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), opts);
+}
